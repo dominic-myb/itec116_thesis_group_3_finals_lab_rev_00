843008ed064b031297f241539657005101c257af
dominic-myb <desguerra246@gmail.com>
2024-01-25 21:39:23 +0800

    Initial Index Controller

--- assets/js/controller/index.controller.js
+++ assets/js/controller/index.controller.js
@@ -1,4 +1,27 @@
 "use strict"
+
+import Environment from "../environment.js";
+
+const environment = new Environment;
 class IndexController{
-    
+    testJqueryIntegration(){
+        console.log('Hello World from JQuery and JavaScript');
+    }
+    async testApiCall(){
+        const url = '';//add link here
+        console.log(`Retrieving data from ${url}`);
+        await environment.fetchRequest('GET',url,null).then(response => {
+            console.log(response);
+        });
+    }
 }
+
+$(document).ready(function(){
+    const indexController = new IndexController;
+    $('#test-button').click(function (e){
+        e.preventDefault();
+        indexController.testApiCall();
+    });
+});
+
+//TODO display data 
